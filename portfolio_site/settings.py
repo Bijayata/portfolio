f308b88aea4680b11a5fc1fe9ac505d6cdef7835
Bijayata <vijayatarai11@gmail.com>
2025-10-25 17:31:03 +0300

    update

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-l-_qsc*e@!_gy0+dlw_=l$%nbz$#-l%*ne@*cqzsom5u(jc=^e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
