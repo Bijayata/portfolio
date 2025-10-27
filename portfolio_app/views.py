d300ca69aadcd154b6154c1290e3ffdb76168020
Bijayata <vijayatarai11@gmail.com>
2025-10-27 13:28:27 +0300

    update

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,15 +1,4 @@
 from django.shortcuts import render
-# from .models import Profile, Skill, Experience, Education
-
 def index(request):
-    # profile = Profile.objects.first()
-    # skills = Skill.objects.all()
-    # experiences = Experience.objects.all()
-    # educations = Education.objects.all()
     return render(request, 'portfolio_app/index.html')
-    # , {
-    #     'profile': profile,
-    #     'skills': skills,
-    #     'experiences': experiences,
-    #     'educations': educations,
-    # })
+   
